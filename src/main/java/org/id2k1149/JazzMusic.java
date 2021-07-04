f8b49a56febf50636941c3961a27c05038b9a23b
id2k1149 <id2k1149@gmail.com>
2021-07-04 13:42:31 -0400

    lesson09

--- src/main/java/org/id2k1149/JazzMusic.java
+++ src/main/java/org/id2k1149/JazzMusic.java
@@ -1,5 +1,8 @@
 package org.id2k1149;
 
+import org.springframework.stereotype.Component;
+
+@Component()
 public class JazzMusic implements Music{
     @Override
     public String getSong() {
--- src/main/java/org/id2k1149/RockMusic.java
+++ src/main/java/org/id2k1149/RockMusic.java
@@ -1,5 +1,8 @@
 package org.id2k1149;
 
+import org.springframework.stereotype.Component;
+
+@Component()
 public class RockMusic implements Music{
     @Override
     public String getSong() {
