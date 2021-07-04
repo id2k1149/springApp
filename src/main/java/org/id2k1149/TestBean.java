d3c3c2d112f4c63c42a9411ae0b55b3e54a921e9
id2k1149 <id2k1149@gmail.com>
2021-07-04 09:34:39 -0400

    lesson02

---
+++ src/main/java/org/id2k1149/TestBean.java
@@ -0,0 +1,17 @@
+package org.id2k1149;
+
+public class TestBean {
+    private String name;
+
+    public TestBean(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
