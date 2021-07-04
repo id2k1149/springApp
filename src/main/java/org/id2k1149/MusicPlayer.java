a93e4f70dc37bf31d1c8e62a118bf2a9c32c973c
id2k1149 <id2k1149@gmail.com>
2021-07-04 11:45:57 -0400

    lesson06 hw

--- src/main/java/org/id2k1149/MusicPlayer.java
+++ src/main/java/org/id2k1149/MusicPlayer.java
@@ -1,23 +1,17 @@
 package org.id2k1149;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
-    private Music music;
+    private List<Music> musicList = new ArrayList<>();
 
     private String name;
 
     private int volume;
 
-    // IoC
-    public MusicPlayer(Music music) {
-        this.music = music;
-    }
-
     public MusicPlayer() {
 
-    }
-
-    public void setMusic(Music music) {
-        this.music = music;
     }
 
     public String getName() {
@@ -36,7 +30,13 @@ public class MusicPlayer {
         this.volume = volume;
     }
 
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
+    }
+
     public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
+        for(Music music : musicList) {
+            System.out.println("Playing: " + music.getSong());
+        }
     }
 }
